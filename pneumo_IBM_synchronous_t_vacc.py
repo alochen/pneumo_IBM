d9025c8a9cf88258e70f79ffe4f9328f63590901
Alessandra <36984907+alochen@users.noreply.github.com>
2021-01-04 15:58:35 +0000

    Update pneumo_IBM_synchronous_t_vacc.py

--- pneumo_IBM_synchronous_t_vacc.py
+++ pneumo_IBM_synchronous_t_vacc.py
@@ -50,16 +50,16 @@ def new_lambdas(carrier_person, ind_lambdas, prev_col, sigma):
 # import fitted carriage duration params from IBM_paramfitting.py
 seroparams = pd.read_csv(r"//qdrive.dide.ic.ac.uk/homes/al817/Technical/Python/seroparams.csv")
 # DATA FROM 20 YR STEADY STATE SIMULATIIION
-# carriers = pd.read_csv(r"//qdrive/homes/al817/Technical/Python/IBM sims/tot population/carriers_fullpop_migr_20yrs_0111.csv")
-# carriers = carriers.drop(['Unnamed: 0'], axis = 1)
-# carriers.vacc = False
-# prev_col = pd.read_csv(r"//qdrive/homes/al817/Technical/Python/IBM sims/tot population/prevcol_fullpop_migr_20yrs_0111.csv")
-# prev_col = prev_col.drop(['Unnamed: 0'], axis = 1)
-# ind_lambdas = prev_col.copy(deep = True)
-# output = pd.read_csv(r"//qdrive/homes/al817/Technical/Python/IBM sims/tot population/output_fullpop_migr_20yrs_0111.csv")
-# output.columns = ['key', 'value']
-# sero_carr = output.drop([0, 72])
-# t = 7299
+carriers = pd.read_csv(r"//qdrive/homes/al817/Technical/Python/IBM sims/tot population/carriers_fullpop_migr_20yrs_0111.csv")
+carriers = carriers.drop(['Unnamed: 0'], axis = 1)
+carriers.vacc = False
+prev_col = pd.read_csv(r"//qdrive/homes/al817/Technical/Python/IBM sims/tot population/prevcol_fullpop_migr_20yrs_0111.csv")
+prev_col = prev_col.drop(['Unnamed: 0'], axis = 1)
+ind_lambdas = prev_col.copy(deep = True)
+output = pd.read_csv(r"//qdrive/homes/al817/Technical/Python/IBM sims/tot population/output_fullpop_migr_20yrs_0111.csv")
+output.columns = ['key', 'value']
+sero_carr = output.drop([0, 72])
+t = 7299
 #sero_carr.value.sort()
 colnames = np.unique(seroparams['serotype']) #list(map(lambda x: 'serotype' + str(x+1), range(num_sero)))
 
@@ -339,166 +339,6 @@ plotname = "simulation" + str(simnum) + ".pdf"
 
 # comment this out for cluster
 #plot.get_figure().savefig(plotname, bbox_inches = "tight")
-
-# POST-VACCINATION
-
-vacc_stop_t <- 4000
-
-while t <= stop_t and tot_lambda1 != 0:
-    
-    if len(np.where(carriers.age > 36500)[0]) > 0:
-        ind_to_die = np.where(carriers.age > 36500)[0]
-        carriers.loc[ind_to_die] = 0 # making this person a baby now
-        carriers.loc[ind_to_die, 'susc'] = max_carr_cap # susc = max carrying capacity when nothing is carried
-        carriers.loc[ind_to_die, 'vacc'] = False
-        prev_col.loc[ind_to_die] = 0
-        
-    index_children = np.where(carriers.age <= 6205)[0]
-    index_adults = np.where((carriers.age > 6205) & (carriers.age <= 23360))[0]
-    index_elderly = np.where(carriers.age > 23360)[0]
-    
-    carriers.loc[index_children, 'agegroup'] = 0
-    carriers.loc[index_adults, 'agegroup'] = 1
-    carriers.loc[index_elderly, 'agegroup'] = 2
-        
-    # add NAs to end of output dataframe if there is no space left to fill
-    if next_ev > currentLength:
-        plotsy = plotsy.append(pd.DataFrame(data = np.nan, index = range(0,chunkSize), columns = plotsy.columns), ignore_index = True)
-        currentLength = currentLength + chunkSize
-    
-    # only record data when t > 9000 (i.e. last 3 years of time)
-    #if t > 9000: 100 # only record after 100th day
-    plotsy.iloc[next_ev-1, 1:] = np.array(carriers.iloc[:,0:num_sero].sum(axis = 0))
-    plotsy.time[next_ev-1] = t
-    
-    if t >= 7300:
-        index_vacc = np.where((carriers.age >= 730) & (carriers.age <= 1095) & (carriers.vacc == False)) # index of ppl to be vaccinated
-        num_newvacc = round(vacc_eff*len(index_vacc[0])) # number of new people to vaccinate (= vacc efficacy * tot number of people)
-        adjusted_ind = index_vacc[0][0:num_newvacc] # indices of people for whom vaccination works/is protective
-        carriers.loc[adjusted_ind, 'vacc'] = True
-    
-    #if t > 3650:
-    #    carriers_all.vacc[(carriers_all.age >= 730) & (carriers_all.age <= 1095)] = True
-        
-    #migration: add 1 carrier to serotypes with zero carriers
-    extinctsero_child = np.where(carriers.iloc[index_children,0:num_sero].sum(axis = 0) == 0)[0]
-    totchildcarr = carriers.iloc[index_children,0:num_sero].sum(axis = 0)
-    totchildcarr[extinctsero_child] = 1
-    
-    extinctsero_adult = np.where(carriers.iloc[index_adults,0:num_sero].sum(axis = 0) == 0)[0]
-    totadultcarr = carriers.iloc[index_adults,0:num_sero].sum(axis = 0)
-    totadultcarr[extinctsero_adult] = 1
-    
-    extinctsero_elder = np.where(carriers.iloc[index_elderly,0:num_sero].sum(axis = 0) == 0)[0]
-    toteldercarr = carriers.iloc[index_elderly,0:num_sero].sum(axis = 0)
-    toteldercarr[extinctsero_elder] = 1
-    
-    totFOIbysero_children = [0]*num_sero
-    totFOIbysero_adults = [0]*num_sero
-    totFOIbysero_elders = [0]*num_sero
-    
-    for i in range(len(seroparams)):
-        totFOIbysero_children[i] = (contact_matrix[0][0]*seroparams['beta_child'][i]*totchildcarr[i]+ 
-                                    contact_matrix[0][1]*seroparams['beta_adult'][i]*totadultcarr[i]+
-                                    contact_matrix[0][2]*seroparams['beta_elder'][i]*toteldercarr[i])
-        totFOIbysero_adults[i] = (contact_matrix[1][0]*seroparams['beta_child'][i]*totchildcarr[i]+  
-                                  contact_matrix[1][1]*seroparams['beta_adult'][i]*totadultcarr[i]+ 
-                                  contact_matrix[1][2]*seroparams['beta_elder'][i]*toteldercarr[i])
-        totFOIbysero_elders[i] =  (contact_matrix[2][0]*seroparams['beta_child'][i]*totchildcarr[i]+ 
-                                    contact_matrix[2][1]*seroparams['beta_adult'][i]*totadultcarr[i]+ 
-                                    contact_matrix[2][2]*seroparams['beta_elder'][i]*toteldercarr[i])
-   
-    totFOIbysero = np.row_stack((totFOIbysero_children, totFOIbysero_adults, totFOIbysero_elders))
-    
-    #totalFOI_children = (contact_matrix[0][0]*B_rates_child_val*totchildcarr +
-    #                     contact_matrix[0][1]*B_rates_adult_val*totadultcarr +
-    #                     contact_matrix[0][2]*B_rates_elder_val*toteldercarr)
-    #totalFOI_adults = (contact_matrix[1][0]*B_rates_child_val*totchildcarr + 
-    #                   contact_matrix[1][1]*B_rates_adult_val*totadultcarr +
-    #                   contact_matrix[1][2]*B_rates_elder_val*toteldercarr)
-    #totalFOI_elderly = (contact_matrix[2][0]*B_rates_child_val*totchildcarr + 
-    #                    contact_matrix[2][1]*B_rates_adult_val*totadultcarr +
-    #                    contact_matrix[2][2]*B_rates_elder_val*toteldercarr)
-    #totFOIbysero = np.row_stack((totalFOI_children, totalFOI_adults, totalFOI_elderly)) 
-    
-    # individual rates                
-    for s in range(indivs):
-        for j in range(num_sero):
-            prevcol = prev_col.iloc[s,j]
-            alpha = seroparams['alpha_emcee'][j]
-            A = seroparams['A_emcee'][j]
-            B = seroparams['B_emcee'][j]
-            # if person not carrying
-            if carriers.iloc[s,j] == 0:
-                # if person is vaccinated & serotype is VT
-                if carriers.vacc[s] == True and np.isin(j,vt_ind):
-                    ind_lambdas.iloc[s,j] = 1/vacc_dur
-                # if serotype is NVT or if person is not vaccinated
-                else:
-                    ind_lambdas.iloc[s,j] = totFOIbysero[carriers['agegroup'][s],j]
-            # if person is carrying, then recovery rate as function of prevcol
-            else:
-                ind_lambdas.iloc[s,j] = (1/np.random.gamma(shape = alpha, scale = (A*np.exp(-prevcol*B))/alpha))                
-    
-    # tot.lambda = total rate of all/any events
-    i = 0
-    U0 = np.random.uniform(0,1, size = 20)
-    U3 = np.random.uniform(0,1, size = 20)
-    while i < 20: # update 50 individuals each time step
-        tot_lambda1 = ind_lambdas.sum().sum()
-        #print(tot_lambda1)
-        U1 = U0[i]
-        U1_tot_lambda1 = U1*tot_lambda1
-        
-        # choose person index ( = which person will this happen to?)
-        cumsum_person = ind_lambdas.sum(axis = 1)
-        which_person = np.where(U1_tot_lambda1 < np.cumsum(cumsum_person))[0][0]
-        #try:
-        #    which_person = np.where(U1_tot_lambda1 < np.cumsum(cumsum_person))[0][0]
-        #except IndexError:
-        #    which_person = np.where(np.random.uniform(0,1)*ind_lambdas.sum().sum() < np.cumsum(cumsum_person))[0][0]#which_person = indivs - 1 #
-        #    break
-        #print(which_person)
-        
-        # choose event that will take place (= which serotype will be toggled?)
-        person_lambdas = new_lambdas(carriers.loc[which_person], ind_lambdas.loc[which_person], prev_col.loc[which_person], sigma)
-            
-        if np.any(person_lambdas < 0):
-            print('NEGATIVE IND_LAMBDA')
-            print("indiv:", which_person)
-            print("carr status:", carriers.iloc[which_person, np.where(person_lambdas < 0)[0][0]])
-            print("serotype:", colnames[np.where(person_lambdas < 0)])
-            print("prevcol:",prev_col.iloc[which_person, np.where(person_lambdas < 0)[0][0]])
-            break
-        tot_lambda2 = sum(person_lambdas)
-        U2 = U3[i]
-        U2_tot_lambda2 = U2*tot_lambda2
-
-        which_indices = np.where(U2_tot_lambda2 < np.cumsum(person_lambdas))
-        
-        #midtime6 = time.time()
-        
-        # switching event (S --> I or I --> S)
-        if which_indices[0].size > 0:
-            which_sero = which_indices[0][0]
-            carriers.iloc[which_person, which_sero] = ~carriers.iloc[which_person, which_sero] + 2        
-
-        #print(which_sero)
-        # reupdate the susc score
-        new_susc = max_carr_cap - sum(carriers.iloc[which_person, 0:num_sero])
-    
-        # update colonisation history if person acquired new serotype    
-        if new_susc < carriers.susc.loc[which_person]:
-            prev_col.iloc[which_person, which_sero] = prev_col.iloc[which_person, which_sero] + 1
-            
-        carriers.loc[carriers.index == which_person,'susc'] = new_susc
-        i += 1
-
-    # update time and age
-    t += 1
-    next_ev += 1
-    carriers.age += 1
-
 
 
 ####### Testing with dummy carriage data ########################################################################################################################
